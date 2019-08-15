13261d3c8b80eb2791da655e1b93bd4ac3e9629e
Kemarus <>
2019-08-15 18:21:17 +0200

    Remove PDF preview in ranger

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -58,11 +58,11 @@ handle_extension() {
             exit 1;;
 
         # PDF
-        pdf)
+        #pdf)
             # Preview as text conversion
-            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - && exit 5
-            exiftool "${FILE_PATH}" && exit 5
-            exit 1;;
+            #pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - && exit 5
+            #exiftool "${FILE_PATH}" && exit 5
+            #exit 1;;
 
         # BitTorrent
         torrent)
